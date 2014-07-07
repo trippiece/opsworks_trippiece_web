e79cb4ab455771237c35464103eda758bdb461bd
Yasuhisa Yamazaki <1@yasu21.net>
2014-07-07 23:05:02 +0900

    add ssh_wrapper for git.

--- api/recipes/default.rb
+++ api/recipes/default.rb
@@ -51,6 +51,7 @@ git "#{node[:app][:directory]}/#{node[:app][:host]}" do
   action :sync
   user node[:app][:owner]
   group node[:app][:group]
+  ssh_wrapper "ssh -i /home/ec2-user/.ssh/id_rsa"
 end
 
 

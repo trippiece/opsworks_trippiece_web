3aef14ae7b7c68bd33ce30a023196c7efddf4ee1
Yasuhisa Yamazaki <1@yasu21.net>
2014-07-07 22:09:28 +0900

    Clean up attributes.

--- api/attributes/default.rb
+++ api/attributes/default.rb
@@ -1,5 +1,2 @@
-default['virtualenv']['parent'] = '/var/virtualenv'
-default['virtualenv']['path'] = "#{default['virtualenv']['parent']}/#{default['app']['host']}"
-default["gunicorn"]["virtualenv"] = default['virtualenv']['path']
 default['app']['owner'] = "ec2-user"
 default['app']['group'] = "ec2-user"

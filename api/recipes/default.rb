2abef4e1ec61f897d6ecf7522566924fd791f1cf
Yasuhisa Yamazaki <1@yasu21.net>
2014-07-09 21:23:00 +0900

    Fix td-agent not being able to read syslog.

--- api/recipes/default.rb
+++ api/recipes/default.rb
@@ -141,15 +141,31 @@ end
 
 # td-agent
 include_recipe 'td-agent'
-
+# create position file directory.
 directory '/var/lib/fluentd' do
   owner 'td-agent'
   group 'td-agent'
   action :create
 end
-
+# add td-agent hack
+cookbook_file 'rsyslog.conf' do
+  path '/etc/rsyslog.conf'
+  action :create
+end
+# loosen permissions
+%w{/var/log/messages /var/log/secure}.each do |file_path|
+  file file_path do
+    mode 0644
+    action :create
+  end
+end
+# restart rsyslog
+service 'rsyslog' do
+  action :restart
+end
+# configuration
 template "/etc/td-agent/td-agent.conf" do
   source 'td-agent.conf.erb'
   action :create
-  notifies :restart, "service[td-agent]", :delayed
+  # no need to notify since the template is subscribed.
 end

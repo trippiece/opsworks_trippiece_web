59353f3bdc15d00413d240fe7f433f1b3f53377a
Yasuhisa <misc@yasu21.net>
2014-07-11 12:01:54 +0900

    Put Retrip draft

---
+++ retrip/attributes/default.rb
@@ -0,0 +1,3 @@
+default['app']['owner'] = "ec2-user"
+default['app']['group'] = "ec2-user"
+default['celery']['config'] = '/etc/default/celeryd'
---
+++ retrip/recipes/deploy.rb
@@ -0,0 +1,40 @@
+app_directory = "#{node[:app][:directory]}/#{node[:app][:host]}"
+
+# deploy git repository.
+git app_directory do
+  repository node[:app][:repository]
+  revision "master"
+  user node[:app][:owner]
+  group node[:app][:group]
+  ssh_wrapper node[:sshignorehost][:path]
+  action :sync
+end
+
+
+# pip install
+bash "pip install -r requirements.txt" do
+  cwd app_directory
+  code <<-EOC
+  #{node[:virtualenv][:path]}/bin/pip install -r requirements.txt
+  EOC
+end
+
+
+# place credential files.
+template "#{app_directory}/#{node[:app][:name]}/#{node[:app][:name]}/settings_base_credential.py" do
+  source 'settings_base_credential.py.erb'
+  action :create
+end
+
+template "#{app_directory}/#{node[:app][:name]}/#{node[:app][:name]}/#{node[:app][:credential]}" do
+  source 'settings_env_credential.py.erb'
+  action :create
+end
+
+
+# restart supervisor services.
+%W{gunicorn-#{node[:app][:name]} celeryd-#{node[:app][:name]}}.each do |srv|
+  supervisor_service srv do
+    action :restart
+  end
+end

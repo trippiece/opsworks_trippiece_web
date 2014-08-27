09ded184c1cb342da6ef9687940d0a12f4a352e6
Yasuhisa Yamazaki <yasu@amrael.net>
2014-08-27 15:11:27 +0900

    add libjpeg-devel

--- retrip/recipes/default.rb
+++ retrip/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'common::virtualenv'
 
 # install python and other required packages.
-%w{mysql mysql-devel libmemcached libmemcached-devel npm}.each do |pkg|
+%w{mysql mysql-devel libmemcached libmemcached-devel npm libjpeg-devel}.each do |pkg|
   package pkg do
     action :upgrade
   end

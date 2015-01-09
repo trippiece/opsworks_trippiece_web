c75619c35eb364edb2f839c8912e0659d089635f
Yasuhisa Yamazaki <1@yasu21.net>
2015-01-09 14:51:40 +0900

    Add libmemcached packages for the web.

--- web/recipes/default.rb
+++ web/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'common::virtualenv'
 
 # install python and other required packages.
-%w{mysql mysql-devel npm libjpeg-devel}.each do |pkg|
+%w{mysql mysql-devel npm libmemcached libmemcached-devel libjpeg-devel}.each do |pkg|
   package pkg do
     action :upgrade
   end

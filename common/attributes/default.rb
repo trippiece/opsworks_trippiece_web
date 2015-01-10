a5c368e84c826d5730e7d0a29f72403dac688e42
Yasuhisa Yamazaki <1@yasu21.net>
2015-01-10 16:21:34 +0900

    Tune keepalive_timeout of nginx.

--- common/attributes/default.rb
+++ common/attributes/default.rb
@@ -2,3 +2,5 @@ default['app']['owner'] = "ec2-user"
 default['app']['group'] = "ec2-user"
 default['app']['revision'] = 'master'
 default['nginx']['cookbook'] = 'nginx'
+default['nginx']['keepalive'] = 'on'
+default['nginx']['keepalive_timeout'] = 2

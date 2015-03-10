a98715e7ce44c562361df80dd64f0d3fa9a87b2b
Yasuhisa Yamazaki <1@yasu21.net>
2015-03-10 15:34:57 +0900

    Add app.default_server_name option. Block requests without valid domain name.

--- retrip/attributes/default.rb
+++ retrip/attributes/default.rb
@@ -1,4 +1,5 @@
 default['app']['api_host'] = ''
+default['app']['default_server_name'] = ''
 default['app']['revision'] = 'master'
 override['nginx']['cookbook'] = 'retrip'
 default['nginx']['custom_block_ips'] = []

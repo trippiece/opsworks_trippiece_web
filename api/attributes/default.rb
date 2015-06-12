be5d4ae0b5879dcf211318c853b3fa4b64ceddff
Yasuhisa Yamazaki <1@yasu21.net>
2015-06-12 16:30:54 +0900

    Enable CONN_MAX_AGE.

--- api/attributes/default.rb
+++ api/attributes/default.rb
@@ -1,2 +1,3 @@
 default['celery']['config'] = '/etc/default/celeryd'
 default['app']['revision'] = 'master'
+default['db']['conn_max_age'] = 60
--- retrip/attributes/default.rb
+++ retrip/attributes/default.rb
@@ -2,6 +2,7 @@ default['app']['api_host'] = ''
 default['app']['default_server_name'] = ''
 default['app']['revision'] = 'master'
 default['app']['grunt_target'] = 'deploy'
+default['db']['default']['conn_max_age'] = 60
 override['nginx']['cookbook'] = 'retrip'
 default['nginx']['custom_block_ips'] = []
 default['celery']['config'] = '/etc/default/celeryd'

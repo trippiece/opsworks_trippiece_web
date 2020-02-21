ab6df9d114e623f1c36ef64428df13da3706c88e
m.dolbniak <m.dolbniak@trippiece.com>
2020-02-21 10:57:19 +0900

    Fix

--- common/recipes/repository.rb
+++ common/recipes/repository.rb
@@ -37,5 +37,6 @@ bash "pip install -r requirements.txt" do
   group node[:app][:group]
   code <<-EOC
   export HOME=~#{node[:app][:owner]}
+  #{node[:virtualenv][:path]}/bin/pip install -r requirements.txt
   EOC
 end
